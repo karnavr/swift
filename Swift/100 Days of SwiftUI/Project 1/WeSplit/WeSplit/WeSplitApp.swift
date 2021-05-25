862816cb8b4dddef4c66b3b7026008c7d93512a7
Karnav Raval <65321583+karnavr@users.noreply.github.com>
2021-05-25 13:27:41 -0400

    create app one project

---
+++ Swift/100 Days of SwiftUI/Project 1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Karnav Raval on 2021-05-25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
