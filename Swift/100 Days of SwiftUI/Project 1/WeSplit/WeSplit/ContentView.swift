804935ae704ff023d4f77800ed19ae01cafaad07
karnavr <kraval3@uwo.ca>
2021-05-25 18:22:01 -0400

    create initial page of WeSplit

--- Swift/100 Days of SwiftUI/Project 1/WeSplit/WeSplit/ContentView.swift
+++ Swift/100 Days of SwiftUI/Project 1/WeSplit/WeSplit/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberofPeople = 2
+    @State private var tipPercentage = 1
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        // calculate total per person here
+        let peopleCount = Double(numberofPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount/100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal/peopleCount
+        
+        return amountPerPerson
+    }
     
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberofPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("tip percentage")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                        .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Total per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            
+            .navigationBarTitle("WeSplit")
+        }
     }
 }
 
