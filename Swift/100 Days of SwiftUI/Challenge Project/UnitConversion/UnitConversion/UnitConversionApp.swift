292a0e7d51adffd40cef36f8815c30832043c36f
karnavr <kraval3@uwo.ca>
2021-05-26 18:30:26 -0400

    create challenge project

---
+++ Swift/100 Days of SwiftUI/Challenge Project/UnitConversion/UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Karnav Raval on 2021-05-26.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
