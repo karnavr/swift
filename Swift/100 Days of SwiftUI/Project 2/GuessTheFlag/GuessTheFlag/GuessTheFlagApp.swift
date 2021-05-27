bf8b7c8655b5e42b4537e973a939367a56b90d88
karnavr <kraval3@uwo.ca>
2021-05-26 22:40:18 -0400

    create project 2

---
+++ Swift/100 Days of SwiftUI/Project 2/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Karnav Raval on 2021-05-26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
