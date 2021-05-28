95c440c3b807adde1d741c103fe7c82d557c6459
karnavr <kraval3@uwo.ca>
2021-05-27 22:15:59 -0400

    finsh project 2 + challenges

--- Swift/100 Days of SwiftUI/Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Swift/100 Days of SwiftUI/Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,9 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var totalScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                        showingScore = true
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                
+                Spacer()
+                HStack(spacing: 15) {
+                    Text("Score:")
+                        .foregroundColor(.white)
+                    Text("\(totalScore)")
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                }
+                Spacer()
+                
+                
+                
+            }
+        }
+        
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is \(totalScore)"), dismissButton: .default(Text("Continue")) {
+                    self.askQuestion()
+            })
+        }
+        
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            totalScore += 1
+        } else {
+            scoreTitle = "False"
+        }
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
@@ -19,3 +88,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// Next Steps:
+// - better the design (tweak spacing of the UI elements, better background colour, etc.)
+// - add a button for resetting the score
+// - tell the player what the correct answer is when they get it wrong
+// - add a heading/title of the game
