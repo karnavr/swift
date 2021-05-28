dea0925e261d50b278f1b8673dd406320ba65ebf
karnavr <kraval3@uwo.ca>
2021-05-27 22:57:28 -0400

    make some custom additions

--- Swift/100 Days of SwiftUI/Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Swift/100 Days of SwiftUI/Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -15,55 +15,72 @@ struct ContentView: View {
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var alertMessage = ""
     @State private var totalScore = 0
     
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
-            
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                }
+        NavigationView {
+            ZStack {
+//                LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+//
+//                Color(.white).edgesIgnoringSafeArea(.all)
                 
-                ForEach(0..<3) { number in
-                    Button(action: {
-                        self.flagTapped(number)
-                        showingScore = true
-                    }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                            .shadow(color: .black, radius: 2)
+                VStack(spacing: 30) {
+                    VStack {
+//                        Text("Tap the flag of")
+//                        Spacer()
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
                     }
+                    
+                    ForEach(0..<3) { number in
+                        Button(action: {
+                            self.flagTapped(number)
+                            showingScore = true
+                        }) {
+                            Image(self.countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                                .shadow(color: .black, radius: 2)
+                        }
+                    }
+                    
+                    Spacer()
+                    HStack(spacing: 15) {
+                        Text("Score:")
+                        Text("\(totalScore)")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                    }
+                    
+                    
+                    Button(action: {totalScore = 0}, label: {
+                        Text("Reset score")
+                            .fontWeight(.bold)
+                            .textCase(.uppercase)
+                            .font(.subheadline)
+                            .padding(.all, 12)
+                            .foregroundColor(.white)
+                            .background(Color.red)
+                            .clipShape(Capsule())
+                    })
+                        
+                        
                 }
-                
-                Spacer()
-                HStack(spacing: 15) {
-                    Text("Score:")
-                        .foregroundColor(.white)
-                    Text("\(totalScore)")
-                        .foregroundColor(.white)
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                }
-                Spacer()
-                
-                
+                .padding(40)
                 
             }
+            .navigationTitle("Guess the Flag")
+            
+            .alert(isPresented: $showingScore) {
+                Alert(title: Text(scoreTitle),
+                      message: Text(alertMessage),
+                      dismissButton: .default(Text("Continue")) {
+                        self.askQuestion()
+                })
         }
-        
-        .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is \(totalScore)"), dismissButton: .default(Text("Continue")) {
-                    self.askQuestion()
-            })
         }
         
     }
@@ -71,9 +88,12 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            alertMessage = "Good job!"
             totalScore += 1
         } else {
             scoreTitle = "False"
+            alertMessage = "Oops! That is actually \(countries[number])'s flag!"
+            
         }
     }
     
@@ -81,11 +101,14 @@ struct ContentView: View {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
     }
 }
 
@@ -93,4 +116,6 @@ struct ContentView_Previews: PreviewProvider {
 // - better the design (tweak spacing of the UI elements, better background colour, etc.)
 // - add a button for resetting the score
 // - tell the player what the correct answer is when they get it wrong
+// - tell the player what country's flag they tapped when they get it wrong
 // - add a heading/title of the game
+// - only show alert if the question was answered wrong
