c1ae2369fca517b3a338193f4529217026a53d53
karnavr <kraval3@uwo.ca>
2021-05-31 19:19:20 -0400

    create project 5

---
+++ Swift/100 Days of SwiftUI/Project 5/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Karnav Raval on 2021-05-31.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
