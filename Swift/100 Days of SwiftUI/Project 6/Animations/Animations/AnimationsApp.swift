bcd3e18cd32b2676fcb60693fc892e200782cc26
karnavr <kraval3@uwo.ca>
2021-06-03 14:36:52 -0400

    create animations project

---
+++ Swift/100 Days of SwiftUI/Project 6/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Karnav Raval on 2021-06-03.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
